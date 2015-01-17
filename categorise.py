bdd815267327eab3952cb3e333239de887421485
Ieuan Jones <ieujon@googlemail.com>
2015-01-17 21:32:39 +0000

    Added Bayesian filter  - Used logarithmic algorithm from wikipedia to avoid floating point underflow  - Trained the database further, still needs a tonne more training though.

--- categorise.py
+++ categorise.py
@@ -27,6 +27,7 @@
 """
 
 import sqlite3
+import math
 import sys
 
 def buildDB():
@@ -42,7 +43,9 @@ def buildDB():
         CREATE TABLE vars (
             name TEXT PRIMARY KEY NOT NULL,
             value INTEGER);
-        INSERT INTO vars (name, value) VALUES ("Total articles", 0)""")
+        INSERT INTO vars (name, value) VALUES ("Total articles", 0);
+        INSERT INTO vars (name, value) VALUES ("Total sport", 0);
+        INSERT INTO vars (name, value) VALUES ("Total not sport", 0);""")
     
     conn.commit()
     conn.close()
@@ -61,30 +64,101 @@ def insert_words(words, category):
     
     if category == "y":
         c.executemany("UPDATE words SET sport = (sport + 1) WHERE word = ?", words)
+        c.execute("UPDATE vars SET value = (value + 1) WHERE name == \"sport\"")
     if category == "n":
         c.executemany("UPDATE words SET not_sport = (not_sport + 1) WHERE word = ?", words)
+        c.execute("UPDATE vars SET value = (value + 1) WHERE name == \"not sport\"")
     
-    c.execute("UPDATE vars SET value = (value + 1) WHERE name == \"Total articles\"")
+    c.execute("UPDATE vars SET value = (value + 1) WHERE name in ('Total articles', 'Total sports', 'Total not sports')")
     
     
     conn.commit()
     conn.close()
 
+def get_word_probabilies():
+    #Turn list of words into tuple
+    conn = sqlite3.connect("words.db")
+    c = conn.cursor()
+    
+    c.execute("SELECT * FROM words")
+    all_words = list(c.fetchall())
+    c.execute("SELECT value FROM vars WHERE name in ('Total articles', 'Total sport', 'Total not sport')")
+    results = list(c.fetchall())
+    total = results[0][0]
+    sports_total = results[1][0]
+    not_sports_total = results[2][0]
+    
+    conn.commit()
+    conn.close()
+    
+    return all_words, total, sports_total, not_sports_total
+
 if "-buildDB" in sys.argv:
     buildDB()
+elif "-guess" in sys.argv:
+    filepath = "../articles/article1.txt"
+    
+    with open(filepath, "r") as article_file:
+        article = article_file.read().decode("utf-8")
+        
+        article = article.encode("ascii", errors='ignore')
+        article = article.replace(".","").replace("?","").replace("!","").replace(",","")
+        
+        words = list(set(article.split()))
+        
+        all_words, total, sports_total, not_sports_total = get_word_probabilies()
+        
+        all_words_dict = {}
+        for i in all_words:
+            all_words_dict[i[1].lower()] = [float(i[2]), float(i[3])]
+        
+        #Bayesian bit
+        
+        n = 0
+        for i in words:
+            if i.lower() in all_words_dict:
+                p_ws = all_words_dict[i.lower()][0] / sports_total
+                p_wns = all_words_dict[i.lower()][1] / not_sports_total
+                
+                if p_ws != 0 and p_wns != 0:
+                    p_i = p_ws / float(p_ws + p_wns)
+                    
+                    n += math.log(1-p_i) - math.log(p_i)
+        
+        p_s = 1 / (1 + math.e**n)
+        
+        n = 0
+        for i in words:
+            if i.lower() in all_words_dict:
+                p_ws = all_words_dict[i.lower()][0] / sports_total
+                p_wns = all_words_dict[i.lower()][1] / not_sports_total
+                
+                if p_ws != 0 and p_wns != 0:
+                    p_i = p_wns / float(p_ws + p_wns)
+                    
+                    n += math.log(1-p_i) - math.log(p_i)
+        
+        p_ns = 1 / (1 + math.e**n)
+        
+        print p_s, p_ns
+        
+        if p_s > p_ns:
+            print "Sport"
+        else:
+            print "Not sport"
+        
 else:
     #filepath = raw_input("Path to file: ")
     #category = raw_input("Is it a sports article? [Y/N] ")
     
     filepath = "../articles/article1.txt"
-    category = "n"
+    category = "y"
     
     if category.lower() in ["y","n"]:
         with open(filepath, "r") as article_file:
-            article = article_file.read()
+            article = article_file.read().decode("utf-8")
             
             article = article.encode("ascii", errors='ignore')
-            
             article = article.replace(".","").replace("?","").replace("!","").replace(",","")
             
             words = list(set(article.split()))
